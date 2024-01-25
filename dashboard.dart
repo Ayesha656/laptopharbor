bda60c0480d237483a6a4353ead55763633d5642
Ayesha656 <114361691+Ayesha656@users.noreply.github.com>
2024-01-25 12:50:58 +0500

    Update dashboard.dart

--- dashboard.dart
+++ dashboard.dart
@@ -1,75 +1,173 @@
-import 'package:flutter/material.dart';
-class dashboard extends StatefulWidget {
-  const dashboard({super.key});
+import'package:flutter/material.dart';
+class dasboard extends StatefulWidget {
+  const dasboard({super.key});
 
   @override
-  State<dashboard> createState() => _dashboardState();
+  State<dasboard> createState() => _dasboardState();
 }
 
-class _dashboardState extends State<dashboard> {
+class _dasboardState extends State<dasboard> {
+  var height, width;
+  List imgsrc= [
+    "images/Achievements.jpg",
+    "images/alerts.jpg",
+    "images/exam.png",
+    "images/workshop.png",
+
+  ];
+  List titles = [
+    "Add",
+    "Fetch",
+    "Remove",
+    "Update"
+  ];
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          // Search Bar
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
+    height= MediaQuery.of(context).size.height;
+    width= MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        width: width,
+        height: height,
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.black
+              ),
+              height: height*0.25,
+              width: width,
+              child: Column(
+
                 children: [
-                  Icon(Icons.search),
-                  SizedBox(width: 8.0),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Search...',
-                        border: InputBorder.none,
-                      ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 35,
+                      left: 15,
+                      right: 15
                     ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                      InkWell(
+                        onTap: (){},
+                        child: Icon(
+                          Icons.sort,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                      ),
+                      Container(
+                        height:40,
+                        width:40,
+                         decoration:  BoxDecoration(
+                           borderRadius: BorderRadius.circular(15),
+                           color: Colors.white,
+                           image: DecorationImage(
+                             image: AssetImage("images/1.png")
+                           )
+                         ),
+
+                      )
+                    ],),
                   ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 20,
+                      left: 30,
+
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Dashboard",
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: 1
+                        ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text("Last Updated 24 Jan 2024",
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white54,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: 1
+                          ),
+                        )
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
-          ),
-          SizedBox(height: 16.0),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: height*0.1),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)
+                  )
+              ),
+              height: height*0.75,
+              width: width,
+              child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1.1,
+                    mainAxisSpacing: 25
+                  ),
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 4,
+                  itemBuilder: (context, index){
+                    return InkWell(
+                      onTap: (){} ,
+                      child: Container(
+                        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              spreadRadius: 1,
+                              blurRadius: 6
+                            )
+                          ],
 
-          // Containers with Image and Text
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              buildContainer('Add Data', Icons.home),
-              buildContainer('Update Data', Icons.business),
-              buildContainer('Delete Data', Icons.school),
-              buildContainer('Fetch Data', Icons.shopping_cart),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Image.asset(imgsrc[index],
+                              width: 100,
 
-  Widget buildContainer(String text, IconData icon) {
-    return Container(
-      width: 80.0,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            Text(
+                            titles[index],
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+
+                  })
+            )
+          ],
+        ),
       ),
-      child: Column(
-        children: [
-          SizedBox(height: 8.0),
-          Icon(icon, size: 30.0),
-          SizedBox(height: 8.0),
-          Text(text),
-          SizedBox(height: 8.0),
-        ],
-      ),
+
     );
   }
 }
